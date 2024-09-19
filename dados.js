1fa7435e36c330215f0b63359367039b4a24d27b
Arthur Henrique Parducci <74994451+ArthurParducci@users.noreply.github.com>
2024-09-19 12:06:03 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,28 @@
+let dados = [
+    {
+        titulo: "Tormenta 20: Uma Aventura Épica em Arton",
+        descricao: "Tormenta 20 é o maior e mais popular RPG de fantasia brasileiro, ambientado no mundo rico e detalhado de Arton.Imagine um universo repleto de magia, criaturas fantásticas, cidades prósperas e perigos ocultos, onde você e seus amigos podem criar heróis épicos e viver aventuras inesquecíveis. Em resumo Tormenta 20 é muito mais do que apenas um jogo de RPG.É uma experiência completa que te convida a explorar um mundo fantástico, criar histórias inesquecíveis e fazer amigos para a vida toda.",
+        link: "https://tsrd.fandom.com/pt-br/wiki/Tormenta_20"
+    },
+    {
+        titulo: "Vampiro: A Máscara: Uma Imersão no Mundo das Trevas",
+        descricao: "ampiro: A Máscara é um RPG de horror que te convida a um mundo sombrio e complexo, onde a linha entre o humano e o monstro é tênue. Nele, você assume o papel de um vampiro recém-abraçado, jogado em uma sociedade secreta de criaturas da noite,dividida em clãs com diferentes origens e poderes. Em resumo Vampiro: A Máscara é mais do que um jogo de RPG. É uma experiência que te convida a explorar os lados mais obscuros da humanidade e da imortalidade. Se você busca um jogo com temas maduros, personagens complexos e um mundo cheio de mistérios, Vampiro: A Máscara é a escolha perfeita.",
+        link: "https://velhomundodastrevas.fandom.com/pt-br/wiki/Vampiro:_A_Máscara"
+    },
+    {
+        titulo: "Ordem Paranormal: Investigando o Sobrenatural",
+        descricao: "Ordem Paranormal é um RPG nacional que te convida a um mundo onde o sobrenatural se esconde nas sombras da nossa realidade. Você e seus amigos assumem o papel de investigadores paranormais, encarregados de desvendar mistérios, enfrentar criaturas das trevas e proteger o mundo dos perigos ocultos. Em resumo Ordem Paranormal é um RPG que combina elementos de terror, investigação e aventura, oferecendo uma experiência única e emocionante. Se você é fã de histórias sobrenaturais e gosta de resolver mistérios, este é o jogo ideal para você.",
+        link: "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal_Wiki"
+    },
+    {
+        titulo: "Chamado de Cthulhu: Mergulhando nas Profundezas da Loucura",
+        descricao: "Chamado de Cthulhu é um RPG de terror cósmico que te transporta para um mundo onde o horror se esconde nas sombras da realidade. Inspirado nas obras de H.P. Lovecraft, o jogo te coloca no papel de investigadores que desvendam mistérios sobrenaturais e confrontam os horrores cósmicos que ameaçam a sanidade humana. Em resumo o Chamado de Cthulhu é um RPG que desafia seus limites e te coloca frente a frente com os horrores mais profundos da existência. Se você busca uma experiência de jogo intensa e aterrorizante, Chamado de Cthulhu é a escolha perfeita.",
+        link: "https://pt.wikipedia.org/wiki/Chamado_de_Cthulhu"
+    },
+    {
+        titulo:"Dungeons & Dragons 5ª Edição",
+        descricao:"Dungeons & Dragons 5ª Edição é o RPG de fantasia mais popular do mundo, onde você e seus amigos podem criar heróis épicos e viver aventuras inesquecíveis em reinos mágicos. Dungeons & Dragons 5ª Edição é um jogo de RPG que oferece infinitas possibilidades de aventura e diversão. Se você buscam jogo para estimular sua imaginação, criar laços de amizade e viver histórias épicas, D&D é a escolha perfeita.",
+        link:"https://dungeons.fandom.com/wiki/Main_Page"
+    }
+];
+
